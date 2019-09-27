22bba71fd801c737041037aa7ea37b335b4fdbcf
David Kyle <dkyle@triumf.ca>
2019-09-26 17:13:28 -0700

    Upload first version for working on it at home.

---
+++ AlbionPriceComparer/AlbionPriceComparer/DataSchemaLoader.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace AlbionPriceComparer
+{
+    class DataSchemaLoader
+    {
+        public static void Load()
+        {
+            DataSet ds = new DataSet("ReferenceData");
+            DataTable dt = new DataTable("BaseItemPower");
+            dt.Columns.Add("artefact_type", typeof(string));
+            dt.Columns.Add("teir", typeof(int));
+            dt.Columns.Add("item_power", typeof(int));
+
+            DataRow dr = dt.NewRow();
+            dr["artefact_type"] = "normal";
+            dr["teir"] = 4;
+            dr["item_power"] = 700;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["artefact_type"] = "normal";
+            dr["teir"] = 5;
+            dr["item_power"] = 800;
+            dt.Rows.Add(dr);
+
+            ds.Tables.Add(dt);
+
+            dt = new DataTable("EnchantProgression");
+            dt.Columns.Add("artefact_type", typeof(string));
+            dt.Columns.Add("teir", typeof(int));
+            dt.Columns.Add("enchant_rune", typeof(int));
+            dt.Columns.Add("enchant_soul", typeof(int));
+            dt.Columns.Add("enchant_relic", typeof(int));
+
+            dr = dt.NewRow();
+            dr["artefact_type"] = "normal";
+            dr["teir"] = 4;
+            dr["enchant_rune"] = 100;
+            dr["enchant_soul"] = 100;
+            dr["enchant_relic"] = 80;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["artefact_type"] = "normal";
+            dr["teir"] = 5;
+            dr["enchant_rune"] = 100;
+            dr["enchant_soul"] = 80;
+            dr["enchant_relic"] = 80;
+            dt.Rows.Add(dr);
+
+            ds.Tables.Add(dt);
+
+            ds.WriteXml("xmldoc.xml");
+            ds.WriteXmlSchema("xmlschema.xml");
+        }
+
+        public static DataSet Read()
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXmlSchema("xmlschema.xml");
+            ds.ReadXml("xmldoc.xml");
+            return ds;
+        }
+
+
+        [DataContract]
+        public class Localization
+        {
+            [DataMember(Name = "EN-US")]
+            public string ENUS { get; set; }
+            [DataMember(Name = "DE-DE")]
+            public string DEDE { get; set; }
+            [DataMember(Name = "FR-FR")]
+            public string FRFR { get; set; }
+            [DataMember(Name = "RU-RU")]
+            public string RURU { get; set; }
+            [DataMember(Name = "PL-PL")]
+            public string PLPL { get; set; }
+            [DataMember(Name = "ES-ES")]
+            public string ESES { get; set; }
+            [DataMember(Name = "PT-BR")]
+            public string PTBR { get; set; }
+            [DataMember(Name = "ZH-CN")]
+            public string ZHCN { get; set; }
+            [DataMember(Name = "KO-KR")]
+            public string KOKR { get; set; }
+        }
+
+        [DataContract]
+        public class ItemJSONLookupRow
+        {
+            [DataMember]
+            public string LocalizationNameVariable { get; set; }
+            [DataMember]
+            public Localization LocalizedNames { get; set; }
+            [DataMember]
+            public Localization LocalizedDescriptions { get; set; }
+            [DataMember]
+            public string Index { get; set; }
+            [DataMember]
+            public string UniqueName { get; set; }
+        }
+
+        public enum ArtefactType
+        {
+            Unknown = 0, Normal, Rune, Soul, Relic
+        }
+
+        [DataContract]
+        public class ItemEnchantment
+        {
+            [DataMember(EmitDefaultValue = true)]
+            public int enchantmentLevel { get; set; }
+            [DataMember(EmitDefaultValue = true)]
+            public int itemPower { get; set; }
+        }
+
+        [DataContract]
+        public class ItemEnchantmentBase
+        {
+            [DataMember]
+            public ItemEnchantment[] enchantments { get; set; }
+        }
+
+        // TODO: add twohanded property
+        [DataContract]
+        public class ItemData
+        {
+            [DataMember]
+            public string itemType { get; set; }
+            [DataMember]
+            public string uniqueName { get; set; }
+            [DataMember]
+            public int itemPower { get; set; }
+            [DataMember]
+            public bool twoHanded { get; set; }
+
+            [DataMember]
+            public Localization localizedNames { get; set; }
+
+            [DataMember]
+            public ItemEnchantmentBase enchantments { get; set; }
+
+            public int teir { get; set; }
+
+            [DataMember(Name = "tier")]
+            private string tier_s
+            {
+                get
+                {
+                    return teir.ToString();
+                }
+                set
+                {
+                    teir = Convert.ToInt32(value);
+                }
+            }
+
+            public ItemData()
+            {
+                teir = 0;
+            }
+        }
+
+        public static DataSet CompileData()
+        {
+            //List<ItemJSONLookupRow> filteredList = new List<ItemJSONLookupRow>();
+            ItemJSONLookupRow[] rawData;
+
+            var ser = new DataContractJsonSerializer(typeof(ItemJSONLookupRow[]));
+            using (FileStream fs = new FileStream("items.json", FileMode.Open))
+            {
+                rawData = (ItemJSONLookupRow[])ser.ReadObject(fs);
+            }
+            var filteredList = from item in rawData
+                               where item.UniqueName.StartsWith("T4")
+                               select item;
+
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+            List<ItemData> items = new List<ItemData>(filteredList.Count());
+            var seri = new DataContractSerializer(typeof(ItemData));
+            foreach (ItemJSONLookupRow item in filteredList)
+            {
+                ItemData buffer;
+                var request = HttpWebRequest.Create(@"https://gameinfo.albiononline.com/api/gameinfo/items/" + item.UniqueName + @"/data");
+                try
+                {
+                    using (WebResponse response = request.GetResponse())
+                    {
+                        using (var stream = new StreamReader(response.GetResponseStream()))
+                        {
+                            buffer = (ItemData)serializer.Deserialize(stream, typeof(ItemData));
+                        }
+                    }
+                    items.Add(buffer);
+                }
+                catch (WebException ex)
+                {
+                    //break;
+                    if (!(ex.Status == WebExceptionStatus.ProtocolError && ex.Message.Contains("(404)")))
+                        throw ex;
+                }
+            }
+            //items.GroupBy(x => x.itemType).Select(g => g.First().itemType).ToList();
+            items = (from i in items
+                     where i.itemType == "equipment" || i.itemType == "weapon"
+                     select i).GroupBy(i => i.uniqueName + i.teir).Select(i => i.First()).ToList();
+
+            var ds = new DataSet();
+            using (Stream stream = typeof(AlbionPriceComparer.MainWindow).Assembly.GetManifestResourceStream("AlbionPriceComparer.ReferenceDataSchema.xml"))
+                ds.ReadXmlSchema(stream);
+            using (Stream stream = typeof(AlbionPriceComparer.MainWindow).Assembly.GetManifestResourceStream("AlbionPriceComparer.ReferenceData.xml"))
+                ds.ReadXml(stream);
+
+            var dtItems = ds.Tables["Items"];
+            var dtBase = ds.Tables["BaseItemPower"];
+
+            var dtEnch = ds.Tables["BaseItemPower"].Copy();
+
+            var une = dtBase.AsEnumerable().Where(r => (int)r["teir"] == 4).Join(
+                ds.Tables["EnchantProgression"].AsEnumerable().Where(ep => (int)ep["teir"] == 4),
+                r => (string)((DataRow)r)["artefact_type"],
+                ep => (string)((DataRow)ep)["artefact_type"],
+                (r, ep) => new { artefact_type = (string)r["artefact_type"], item_power = (int)r["item_power"] + (int)ep["enchant_rune"] });
+            dtEnch.Clear();
+            foreach (var a in une)
+            {
+                var r = dtEnch.NewRow();
+                r["teir"] = 4;
+                r["artefact_type"] = a.artefact_type;
+                r["item_power"] = a.item_power;
+                dtEnch.Rows.Add(r);
+            }
+
+            foreach (ItemData i in items)
+            {
+                var row = dtItems.NewRow();
+                row["id"] = i.uniqueName.Replace("T4_", "");
+                row["name"] = i.localizedNames.ENUS.Replace("Adept's ", "");
+                row["two_handed"] = i.twoHanded;
+                if (i.itemPower >= 500 || (i.itemPower < 500 && i.enchantments != null))
+                {
+                    if (i.itemPower < 500)
+                    {
+                        int ip = i.itemPower;
+                        ip = i.enchantments.enchantments.Where(x => x.enchantmentLevel == 1).First().itemPower;
+                        row["artefact_type"] = ArtefactTypeLookup(dtEnch, 4, ip);
+                    }
+                    else
+                    {
+                        row["artefact_type"] = ArtefactTypeLookup(dtBase, 4, i.itemPower);
+                    }
+                    dtItems.Rows.Add(row);
+                }
+            }
+
+            return ds;
+        }
+
+        private static ArtefactType ArtefactTypeLookup(DataTable dt, int teir, int itemPower)
+        {
+            var x = (string)dt.AsEnumerable()
+                .Where(row => (int)row["teir"] == teir && (int)row["item_power"] == itemPower)
+                .First()["artefact_type"];
+            
+            foreach (ArtefactType a in Enum.GetValues(typeof(ArtefactType)))
+            {
+                if (Enum.GetName(typeof(ArtefactType), a).Equals(x, StringComparison.OrdinalIgnoreCase))
+                    return a;
+            }
+            return ArtefactType.Unknown;
+        }
+    }
+}
---
+++ AlbionPriceComparer/AlbionPriceComparer/MainWindow.xaml.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using Swashbuckle;
+using Swashbuckle.Swagger;
+using System.IO;
+using System.Xml;
+using System.Text.RegularExpressions;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Xml.Linq;
+using System.Data;
+using static AlbionPriceComparer.DataSchemaLoader;
+
+namespace AlbionPriceComparer
+{
+    public enum City
+    {
+        Unknown = 0,
+        Caleron = 1,
+        FortSterling = 2,
+        Bridgewatch = 3,
+        Lymhurst = 4,
+        Thetford = 5,
+        Martlock = 6
+    }
+
+    public static class CityMethods
+    {
+        public static string DisplayName(this City city)
+        {
+            switch (city)
+            {
+                case City.Unknown:
+                    return "";
+                case City.FortSterling:
+                    return "Fort Sterling";
+                default:
+                    return Enum.GetName(typeof(City), city);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private enum ItemQuality
+        {
+            Unknown = -1,
+            Normal = 0,
+            Good = 10,
+            Outstanding = 20,
+            Excelent = 50,
+            Masterpeice = 100
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        [DataContract]
+        private class MarketListing
+        {
+            [DataMember]
+            public string item_id { get; set; }
+            [DataMember]
+            public int sell_price_min { get; set; }
+            [DataMember]
+            public int sell_price_max { get; set; }
+            [DataMember]
+            public int buy_price_min { get; set; }
+            [DataMember]
+            public int buy_price_max { get; set; }
+
+            public City city { get; set; }
+
+            [DataMember(Name = "city")]
+            public string city_s
+            {
+                get => city.DisplayName();
+                set
+                {
+                    city = City.Unknown;
+                    string cityName = value.Replace(" ", "").ToLower();
+                    foreach (City id in (City[])Enum.GetValues(typeof(City)))
+                    {
+                        if (Enum.GetName(typeof(City), id).ToLower() == cityName)
+                        {
+                            city = id;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            public ItemQuality quality { get; set; }
+
+            [DataMember(Name = "quality")]
+            private int quality_id
+            {
+                get => ItemQualityIdLookup(quality);
+                set => quality = ItemQualityLookup(value);
+            }
+
+            private static ItemQuality ItemQualityLookup(int iq)
+            {
+                switch (iq)
+                {
+                    case 1:
+                        return ItemQuality.Normal;
+                    case 2:
+                        return ItemQuality.Good;
+                    case 3:
+                        return ItemQuality.Outstanding;
+                    case 4:
+                        return ItemQuality.Excelent;
+                    case 5:
+                        return ItemQuality.Masterpeice;
+                    default:
+                        return ItemQuality.Unknown;
+                }
+            }
+
+            private static int ItemQualityIdLookup(ItemQuality iq)
+            {
+                switch (iq)
+                {
+                    case ItemQuality.Normal:
+                        return 1;
+                    case ItemQuality.Good:
+                        return 2;
+                    case ItemQuality.Outstanding:
+                        return 3;
+                    case ItemQuality.Excelent:
+                        return 4;
+                    case ItemQuality.Masterpeice:
+                        return 5;
+                    default:
+                        return 0;
+                }
+            }
+
+            public DateTime sell_price_min_date { get; set; }
+            public DateTime sell_price_max_date { get; set; }
+            public DateTime buy_price_min_date { get; set; }
+            public DateTime buy_price_max_date { get; set; }
+
+            [DataMember(Name = "sell_price_min_date")]
+            private string sell_price_min_date_s
+            {
+                get => sell_price_min_date.ToString("yyyy-MM-ddTHH:mm:ss");
+                set => sell_price_min_date = Convert.ToDateTime(value);
+            }
+
+            [DataMember(Name = "sell_price_max_date")]
+            private string sell_price_max_date_s
+            {
+                get => sell_price_max_date.ToString("yyyy-MM-ddTHH:mm:ss");
+                set => sell_price_max_date = Convert.ToDateTime(value);
+            }
+
+            [DataMember(Name = "buy_price_min_date")]
+            private string buy_price_min_date_s
+            {
+                get => buy_price_min_date.ToString("yyyy-MM-ddTHH:mm:ss");
+                set => buy_price_min_date = Convert.ToDateTime(value);
+            }
+
+            [DataMember(Name = "buy_price_max_date")]
+            private string buy_price_max_date_s
+            {
+                get => buy_price_max_date.ToString("yyyy-MM-ddTHH:mm:ss");
+                set => buy_price_max_date = Convert.ToDateTime(value);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string stringBuffer;
+
+            using (Stream stream = this.GetType().Assembly.GetManifestResourceStream("AlbionPriceComparer.ReferenceData.xml"))
+            {
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    stringBuffer = sr.ReadToEnd();
+                }
+            }
+
+            //DataSchemaLoader.Load();
+            DataSet ds = DataSchemaLoader.Read();
+            DataSet items = DataSchemaLoader.CompileData();
+
+            WebRequest request;
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+
+            //MarketListing[] listings;
+            List<MarketListing> listings = new List<MarketListing>();
+            request = HttpWebRequest.Create(@"https://www.albion-online-data.com/api/v2/stats/prices/T4_RUNE?locations=Caerleon,Bridgewatch,Fort%20Sterling&qualities=2");
+            using (WebResponse response = request.GetResponse())
+            {
+                var ser = new DataContractJsonSerializer(typeof(MarketListing[]));
+                using (Stream sr = response.GetResponseStream())
+                {
+                    listings.AddRange((MarketListing[])ser.ReadObject(sr));
+                }
+            }
+
+
+            string marker = @"<td>Item Power</td><td>";
+            int cutStart = stringBuffer.IndexOf(marker) + marker.Length;
+            int cutEnd = stringBuffer.IndexOf(@"</td", cutStart);
+            stringBuffer = stringBuffer.Substring(cutStart, cutEnd - cutStart);
+            int baseItemPower = int.Parse(stringBuffer);
+
+
+            //MarketListing[] listings;
+            listings = new List<MarketListing>();
+            request = HttpWebRequest.Create(@"https://www.albion-online-data.com/api/v2/stats/prices/T4_MAIN_RAPIER_MORGANA?locations=Caerleon,Bridgewatch,Fort%20Sterling&qualities=2");
+            using (WebResponse response = request.GetResponse())
+            {
+                var ser = new DataContractJsonSerializer(typeof(MarketListing[]));
+                using (Stream sr = response.GetResponseStream())
+                {
+                    listings.AddRange((MarketListing[])ser.ReadObject(sr));
+                }
+            }
+        }
+
+        public enum EnchantLevel
+        {
+            None = 0, Rune, Soul, Relic
+        }
+
+        private EnchantPrices enchPrices = new EnchantPrices();
+
+        private class EnchantPrices
+        {
+            private double[,] enchPrices = new double[5, 3];
+            private DateTime[,] enchPriceDates = new DateTime[5, 3];
+
+            public double GetPrice(int teir, EnchantLevel lvl, out DateTime dt)
+            {
+                teir -= 4;
+                int b = (int)lvl - 1;
+                dt = enchPriceDates[teir, b];
+                return enchPrices[teir, b];
+            }
+            public void SetPrice(int teir, EnchantLevel lvl, DateTime dt, double val)
+            {
+                teir -= 4;
+                int b = (int)lvl - 1;
+                enchPriceDates[teir, b] = dt;
+                enchPrices[teir, b] = val;
+            }
+        }
+
+        private void BtnEnchantPriceLookup_Click(object sender, RoutedEventArgs e)
+        {
+            string[] enchNames = { "RUNE", "SOUL", "RELIC" };
+            EnchantLevel[] enchLvl = { EnchantLevel.Rune, EnchantLevel.Soul, EnchantLevel.Relic };
+            string[] teirNames = { "T4_", "T5_", "T6_", "T7_", "T8_" };
+            int[] teirs = { 4, 5, 6, 7, 8 };
+
+
+            for (int i = 0; i < enchLvl.Length; i++)
+            {
+                for (int j = 0; j < teirs.Length; j++)
+                {
+                    WebRequest request = HttpWebRequest.Create(@"https://www.albion-online-data.com/api/v2/stats/prices/" + teirNames[j] + enchNames[i] + @"?locations=Caerleon,Fort%20Sterling&qualities=2");
+                    using (WebResponse response = request.GetResponse())
+                    {
+                        var ser = new DataContractJsonSerializer(typeof(MarketListing[]));
+                        MarketListing[] ml;
+
+                        using (Stream sr = response.GetResponseStream())
+                            ml = (MarketListing[])ser.ReadObject(sr);
+
+                        double count = 0;
+                        double sum = 0;
+                        TimeSpan timeDis = new TimeSpan(0);
+                        foreach (MarketListing m in ml)
+                        {
+                            if (m.city != City.Unknown)
+                            {
+                                count++;
+                                sum += m.sell_price_min;
+                                timeDis += DateTime.UtcNow - m.sell_price_min_date;
+                            }
+                        }
+                        DateTime avgDate = DateTime.UtcNow - new TimeSpan(timeDis.Ticks / (long)count);
+                        double avgPrice = sum / count;
+                        enchPrices.SetPrice(teirs[j], enchLvl[i], avgDate, avgPrice);
+                    }
+                }
+            }
+        }
+    }
+}
